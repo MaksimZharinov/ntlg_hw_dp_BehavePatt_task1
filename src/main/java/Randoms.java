8fea1a28eb4fd7ddcf2a42ed573d233f57a89c80
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-04-28 02:41:04 +0700

    change Randoms class

--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -2,13 +2,26 @@ import java.util.Iterator;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
-    protected Random random;
+    protected Random random = new Random();
+    protected int min;
+    protected int max;
 
     public Randoms(int min, int max) {
-
+        this.min = min;
+        this.max = max + 1;
     }
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(min, max);
+            }
+        };
     }
 }
